7993920bf5f6b859d007b47b861ccee0031238c9
Pavel Senin <seninp@gmail.com>
2017-01-05 17:50:32 -0700

    working on the sanity tests, 100 mutated cylinders

--- src/main/java/net/seninp/attractor/experiment/Step01Mutator.java
+++ src/main/java/net/seninp/attractor/experiment/Step01Mutator.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 import org.apache.commons.math3.ode.nonstiff.ClassicalRungeKuttaIntegrator;
@@ -26,6 +28,9 @@ import net.seninp.jmotif.sax.TSProcessor;
 import net.seninp.jmotif.sax.alphabet.Alphabet;
 import net.seninp.jmotif.sax.alphabet.NormalAlphabet;
 import net.seninp.jmotif.sax.datastructure.SAXRecords;
+import net.seninp.jmotif.text.Params;
+import net.seninp.jmotif.text.TextProcessor;
+import net.seninp.jmotif.text.WordBag;
 import net.seninp.util.UCRUtils;
 import us.molini.graph.GraphFactory;
 
@@ -40,13 +45,19 @@ public class Step01Mutator {
   private final static int PAA_SIZE = 6;
   private final static int ALPHABET_SIZE = 6;
   private final static double NORM_THRESHOLD = 0.01;
+  private static final NumerosityReductionStrategy NR_STRATEGY = NumerosityReductionStrategy.NONE;
   private final static Alphabet ALPHABET = new NormalAlphabet();
+  private final static TextProcessor tp = new TextProcessor();
   private final static SAXProcessor sp = new SAXProcessor();
 
   // the data
-  private static final String DATA_FILE = "src/resources/data/CBF/CBF_TRAIN";
+  private static final String TRAIN_DATA = "src/resources/data/CBF/CBF_TRAIN";
+  private static final String TEST_DATA = "src/resources/data/CBF/CBF_TEST";
 
   // the curve
+  private static final double BASE_A = 0.20;
+  private static final double BASE_B = 0.20;
+  private static final double BASE_C = 5.0;
   private static final ClassicalRungeKuttaIntegrator INTEGRATOR = new ClassicalRungeKuttaIntegrator(
       0.01);
 
@@ -57,8 +68,8 @@ public class Step01Mutator {
 
     // 0.0 -- read the data
     //
-    Map<String, List<double[]>> CBFData = UCRUtils.readUCRData(DATA_FILE);
-    LOGGER.info("read " + UCRUtils.datasetStats(CBFData, "\"" + DATA_FILE + "\" "));
+    Map<String, List<double[]>> CBFData = UCRUtils.readUCRData(TRAIN_DATA);
+    LOGGER.info("read " + UCRUtils.datasetStats(CBFData, "\"" + TRAIN_DATA + "\" "));
 
     // 0.1 -- take the only class #1 and series #0 and mutate it ... later
     //
@@ -72,7 +83,7 @@ public class Step01Mutator {
     // 0.2 disretize
     //
     SAXRecords sax = sp.ts2saxViaWindow(series, WINDOW_SIZE, PAA_SIZE,
-        ALPHABET.getCuts(ALPHABET_SIZE), NumerosityReductionStrategy.NONE, NORM_THRESHOLD);
+        ALPHABET.getCuts(ALPHABET_SIZE), NR_STRATEGY, NORM_THRESHOLD);
     ArrayList<Integer> indexes = new ArrayList<Integer>();
     indexes.addAll(sax.getIndexes());
     Collections.sort(indexes);
@@ -92,7 +103,7 @@ public class Step01Mutator {
     // 0.3 generate the curve
     //
     ArrayList<double[]> theCurve = new ArrayList<double[]>();
-    RosslerEquations equations = new RosslerEquations(0.439, 1.99, 3.75);
+    RosslerEquations equations = new RosslerEquations(BASE_A, BASE_B, BASE_C);
     RosslerStepHandler stepHandler = new RosslerStepHandler("e01_original_curve.txt", theCurve);
     INTEGRATOR.addStepHandler(new StepNormalizer(0.1, stepHandler));
     INTEGRATOR.integrate(equations, 0, new double[] { 1., 1., 1. }, theString.length() * 0.1,
@@ -123,34 +134,90 @@ public class Step01Mutator {
     }
     tree.visualize(600, 600).save("target/mytree.png");
 
-    // 0.6 mutate the curve a bit
-    //
-    ArrayList<double[]> theMutatedCurve = new ArrayList<double[]>();
-    equations = new RosslerEquations(0.440, 2.01, 3.74);
-    stepHandler = new RosslerStepHandler("e01_mutated_curve.txt", theMutatedCurve);
-    INTEGRATOR.addStepHandler(new StepNormalizer(0.1, stepHandler));
-    INTEGRATOR.integrate(equations, 0, new double[] { 0.99, 1., 1. }, theString.length(),
-        new double[3]);
+    Hashtable<String, WordBag> mutatedSeries = new Hashtable<String, WordBag>();
 
-    // 0.6 extract the new string
-    //
-    StringBuffer theNewString = new StringBuffer(indexes.size() * PAA_SIZE);
-    for (int i = 0; i < Math.min(theMutatedCurve.size(), theString.length()); i++) {
+    LOGGER.info("producing mutants");
 
-      double[] newP = theMutatedCurve.get(i);
-      com.github.davidmoten.rtree.geometry.Point point = Geometries.point(newP[1], newP[2]);
+    for (int j = 0; j < 100; j++) {
 
-      Entry<String, Geometry> pp = tree.nearest(point, 1, 1).toBlocking().single();
+      // 0.6 mutate the curve a bit
+      //
+      double a = BASE_A + (0.05 - Math.random() / 10.0);
+      double b = BASE_B + (0.05 - Math.random() / 10.0);
+      double c = BASE_C + (0.05 - Math.random() / 10.0);
 
-      theNewString.append(pp.value());
+      ArrayList<double[]> theMutatedCurve = new ArrayList<double[]>();
+      equations = new RosslerEquations(a, b, c);
+      stepHandler = new RosslerStepHandler("e01_mutated_curve.txt", theMutatedCurve);
+      INTEGRATOR.addStepHandler(new StepNormalizer(0.1, stepHandler));
+      INTEGRATOR.integrate(equations, 0, new double[] { 1., 1., 1. }, theString.length(),
+          new double[3]);
+
+      // 0.6 extract the new string
+      //
+      StringBuffer theNewString = new StringBuffer(indexes.size() * PAA_SIZE);
+      for (int i = 0; i < Math.min(theMutatedCurve.size(), theString.length()); i++) {
+        double[] newP = theMutatedCurve.get(i);
+        com.github.davidmoten.rtree.geometry.Point point = Geometries.point(newP[1], newP[2]);
+        Entry<String, Geometry> pp = tree.nearest(point, 3, 1).toBlocking().single();
+        theNewString.append(pp.value());
+      }
+
+      // writer = new PrintWriter(new File("e01_mutated_string.txt"), "UTF-8");
+      // for (int i = 0; i < theNewString.length(); i++) {
+      // writer.println(theNewString.charAt(i));
+      // }
+      // writer.close();
+
+      // save string into a word bag
+      //
+      WordBag wb = new WordBag(key + "_" + String.valueOf(j));
+      int ctr = 0;
+      while (ctr < theNewString.length()) {
+        CharSequence word = theNewString.subSequence(ctr, ctr + PAA_SIZE);
+        wb.addWord(word.toString());
+        ctr = ctr + PAA_SIZE;
+      }
+
+      mutatedSeries.put(wb.getLabel(), wb);
+
+      System.out.println(theNewString.toString());
 
     }
 
-    writer = new PrintWriter(new File("e01_mutated_string.txt"), "UTF-8");
-    for (int i = 0; i < theNewString.length(); i++) {
-      writer.println(theNewString.charAt(i));
+    LOGGER.info("training the classifier");
+
+    Map<String, List<double[]>> trainData = UCRUtils.readUCRData(TRAIN_DATA);
+    Map<String, List<double[]>> testData = UCRUtils.readUCRData(TEST_DATA);
+
+    Params params = new Params(WINDOW_SIZE, PAA_SIZE, ALPHABET_SIZE, NORM_THRESHOLD, NR_STRATEGY);
+
+    List<WordBag> bags = tp.labeledSeries2WordBags(trainData, params);
+
+    HashMap<String, HashMap<String, Double>> tfidf = tp.computeTFIDF(bags);
+
+    // classifying
+    int testSampleSize = 0;
+    int positiveTestCounter = 0;
+
+    for (java.util.Map.Entry<String, WordBag> e : mutatedSeries.entrySet()) {
+
+      String predictedLabel = tp.classify(e.getValue(), tfidf);
+
+      if (predictedLabel.equalsIgnoreCase(key)) {
+        positiveTestCounter++;
+      }
+
+      testSampleSize++;
+
     }
-    writer.close();
+
+    // accuracy and error
+    double accuracy = (double) positiveTestCounter / (double) testSampleSize;
+    double error = 1.0d - accuracy;
+
+    // report results
+    System.out.println("classification results: " + accuracy + "; " + error);
 
   }
 

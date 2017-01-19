d12ee3fe16a933a39db9da3070c86cec9b735ea0
Pavel Senin <seninp@gmail.com>
2017-01-19 15:10:42 -0700

    trying to build a network

--- RCode/UCR_transform/cbf_transform.R
+++ RCode/UCR_transform/cbf_transform.R
@@ -1,6 +1,6 @@
 library(stringr)
 ##
-conn <- file("../src/resources/data/CBF/CBF_TRAIN", open = "r")
+conn <- file("../src/resources/data/CBF/CBF_TEST", open = "r")
 linn <- readLines(conn)
 ##
 ds <- (1:129)
@@ -8,10 +8,10 @@ ds <- (1:129)
 for (i in 1:length(linn)) {
   str <- str_trim(linn[i])
   str_split <- unlist(strsplit(str, "\\s+"))
-  label <- as.numeric(str_split[1])
+  label <- as.numeric(str_split[1]) - 1
   series <- as.numeric(str_split[2:129])
   ds <- rbind(ds, c(series, label))
 }
 close(conn)
 #
-write.csv(ds[-1,], "../src/resources/data/CBF/cbf_train_original.csv", col.names = F, row.names = F)
+write.table(ds[-1,], "../src/resources/data/CBF/cbf_test_original.csv", col.names = F, row.names = F, sep = ",")
---
+++ src/main/java/net/seninp/dl4j/CBFDLTest.java
@@ -0,0 +1,108 @@
+package net.seninp.dl4j;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.split.FileSplit;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.SplitTestAndTrain;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
+import org.nd4j.linalg.dataset.api.preprocessor.NormalizerStandardize;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CBFDLTest {
+
+  // the data
+  private static final String TRAIN_DATA = "src/resources/data/CBF/cbf_train_original.csv";
+
+  private static Logger log = LoggerFactory.getLogger(CBFDLTest.class);
+
+  public static void main(String[] args)
+      throws FileNotFoundException, IOException, InterruptedException {
+
+    RecordReader recordReader = new CBFRecordReader(0, ",");
+
+    // ClassPathResource res = new ClassPathResource(TRAIN_DATA);
+
+    // RecordReader recordReader = new CSVRecordReader(0,",");
+
+    FileSplit files = new FileSplit(new File(TRAIN_DATA));
+
+    recordReader.initialize(files);
+
+    // Second: the RecordReaderDataSetIterator handles conversion to DataSet objects, ready for use
+    // in neural network
+    int labelIndex = 128; // 5 values in each row of the iris.txt CSV: 4 input features followed by
+                          // an
+    // integer label (class) index. Labels are the 5th value (index 4) in each
+    // row
+    int numClasses = 3; // 3 classes (types of iris flowers) in the iris data set. Classes have
+                        // integer values 0, 1 or 2
+    int batchSize = 29; // Iris data set: 150 examples total. We are loading all of them into one
+                        // DataSet (not recommended for large data sets)
+
+    DataSetIterator iterator = new RecordReaderDataSetIterator(recordReader, batchSize, labelIndex,
+        numClasses);
+    DataSet allData = iterator.next();
+    allData.shuffle();
+    SplitTestAndTrain testAndTrain = allData.splitTestAndTrain(0.80); // Use 65% of data for
+                                                                      // training
+
+    DataSet trainingData = testAndTrain.getTrain();
+    DataSet testData = testAndTrain.getTest();
+
+    // We need to normalize our data. We'll use NormalizeStandardize (which gives us mean 0, unit
+    // variance):
+    DataNormalization normalizer = new NormalizerStandardize();
+    normalizer.fit(trainingData); // Collect the statistics (mean/stdev) from the training data.
+                                  // This does not modify the input data
+    normalizer.transform(trainingData); // Apply normalization to the training data
+    normalizer.transform(testData); // Apply normalization to the test data. This is using
+                                    // statistics calculated from the *training* set
+
+    final int numInputs = 128;
+    int outputNum = 3;
+    int iterations = 1000;
+    long seed = 6;
+
+    log.info("Build model....");
+    MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder().seed(seed)
+        .iterations(iterations).activation("relu").weightInit(WeightInit.XAVIER).learningRate(0.1)
+        .regularization(true).l2(1e-4).list()
+        .layer(0, new DenseLayer.Builder().nIn(numInputs).nOut(3).build())
+        .layer(1, new DenseLayer.Builder().nIn(3).nOut(3).build())
+        .layer(2,
+            new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                .activation("softmax").nIn(3).nOut(outputNum).build())
+        .backprop(true).pretrain(false).build();
+
+    // run the model
+    MultiLayerNetwork model = new MultiLayerNetwork(conf);
+    model.init();
+    model.setListeners(new ScoreIterationListener(100));
+
+    model.fit(trainingData);
+
+    // evaluate the model on the test set
+    Evaluation eval = new Evaluation(3);
+    INDArray output = model.output(testData.getFeatureMatrix());
+    eval.eval(testData.getLabels(), output);
+    log.info(eval.stats());
+
+  }
+
+}
